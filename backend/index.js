c6b8a22a0dd5e5e373b0a579dd69ad3a4997ad59
koko <tgwcoco6@gmail.com>
2025-10-07 13:34:31 +0900

    環境構築

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+
+res.send('Hello from Node.js Backend!');
+
+});
+
+app.listen(port, () => {
+
+console.log(`Backend listening at http://localhost:${port}`);
+
+});
