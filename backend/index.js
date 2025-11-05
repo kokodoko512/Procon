028c9f1d1d1d31c0f39f24459aabb6298998a66d
koko <tgwcoco6@gmail.com>
2025-11-05 19:53:28 +0900

    port edited

--- backend/index.js
+++ backend/index.js
@@ -282,7 +282,8 @@ app.get("/api/youtube/songs", async (req, res) => {
 // =========================
 // サーバー起動
 // =========================
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
+
