4cff61e6fe1c4600eb629d7fe8b6c668d6be8ee5
koko <tgwcoco6@gmail.com>
2025-11-05 17:19:53 +0900

    DBのデプロイ

--- backend/db.js
+++ backend/db.js
@@ -3,15 +3,15 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const pool = mysql.createPool({
+const db = await mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT || 3306,
+    ssl: {
+        rejectUnauthorized: true
+    }
 });
 
 export default pool;
