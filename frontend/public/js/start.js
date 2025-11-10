a71e4b4a588a20add8ee117dc69ba6af216991c0
koko <tgwcoco6@gmail.com>
2025-11-10 13:54:30 +0900

    再生画面拡大

--- frontend/public/js/start.js
+++ frontend/public/js/start.js
@@ -24,7 +24,7 @@ document.querySelector(".create-btn").addEventListener("click", async() => {
     // room.htmlに遷移
     setTimeout(() => {
       window.location.href = "room.html";
-    }, 350);
+    }, 500);
 
   } catch (error) {
     console.error("初期化失敗:", error);
